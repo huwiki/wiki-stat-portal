3ceb085a17761ade9cfa15511e3f423e0a0981a0
Tar Dániel <bdanee88@gmail.com>
2021-12-05 22:53:00 +0100

    Add missing last_actor_update to create script

--- server/database/entities/toolsDatabase/createStatisticsTables.sql
+++ server/database/entities/toolsDatabase/createStatisticsTables.sql
@@ -2,6 +2,7 @@ CREATE TABLE `wiki_processed_revisions_v2` (
   `wiki` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_processed_revision_id` int(10) NOT NULL,
   `last_processed_log_id` int(10) NOT NULL,
+  `last_actor_update` datetime NOT NULL,
   PRIMARY KEY (`wiki`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
