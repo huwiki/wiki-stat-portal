e296b70216f41ebc6e68cc716ed37f5eae404405
Tar Dániel <bdanee88@gmail.com>
2021-12-05 22:53:00 +0100

    Add MediaWiki LogEntry entity

---
+++ server/database/entities/mediawiki/logEntry.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { bufferToDateTimeTransformer, bufferToStringTransformer, intToBooleanTransformer } from "../../transformers";
+import { Actor } from "./actor";
+
+@Entity({ name: "logging" })
+export class LogEntry {
+	// rev_id int(10) UN
+	@PrimaryGeneratedColumn({ name: "log_id", type: "int", unsigned: true })
+	public id: number;
+
+	// log_type varbinary(32)
+	@Column({ name: "log_type", type: "varbinary", length: 32, transformer: bufferToStringTransformer })
+	public type: string;
+
+	// log_action varbinary(32)
+	@Column({ name: "log_action", type: "varbinary", length: 32, transformer: bufferToStringTransformer })
+	public action: string;
+
+	// log_timestamp varbinary(14)
+	@Column({ name: "log_timestamp", type: "varbinary", length: 14, scale: 0, transformer: bufferToDateTimeTransformer })
+	public timestamp: Date;
+
+	// log_actor decimal(20,0)
+	@Column({ name: "log_actor", type: "decimal", precision: 20, scale: 0 })
+	public actorId: number;
+
+	// log_actor decimal(20,0)
+	@OneToOne(() => Actor, actor => actor.id)
+	@JoinColumn({ name: "log_actor" })
+	public actor: Actor;
+
+	// log_namespace int(11)
+	@Column({ name: "log_namespace", type: "int", unsigned: true })
+	public namespace: number;
+
+	// log_deleted tinyint(1) UN
+	@Column({ name: "log_deleted", type: "tinyint", unsigned: true, transformer: intToBooleanTransformer })
+	public isDeleted: boolean;
+}
